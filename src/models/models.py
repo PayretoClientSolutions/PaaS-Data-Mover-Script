6d7135a53ec378ad4dc7b31ab4625159e26f724b
Bryan Olandres <ozoneblacklight@outlook.com>
2025-12-05 22:36:56 +0800

    add docstrings to models

--- src/models/models.py
+++ src/models/models.py
@@ -4,6 +4,19 @@ from pathlib import Path
 
 @dataclass
 class SFTPConfig:
+    """
+    configuration for SFTP connections per BIP.
+
+    Attributes:
+        hostname (str): The hostname of the SFTP server.
+        username (str): The username for SFTP authentication.
+        port (int): The port number for the SFTP connection.
+        password (str): The password for SFTP authentication.
+        path_to_key (str): The path to the private key file for key-based authentication.
+        local_path (str): The local directory path for file downloads.
+        target_file_type (str): The target file type for processing, default is ".csv".
+        remote_path (str): The remote directory path on the SFTP server, default is "/REPORTS".
+    """
     hostname: str
     username: str
     port: int
@@ -16,6 +29,14 @@ class SFTPConfig:
 
 @dataclass
 class MoverConfig:
+    """
+    Mover configuration.
+
+    Attributes:
+        working_dir (Path): Directory where files were initially downloaded.
+        sent_dir (Path): Directory where processed files are moved after handling.
+        path_to_gcs_credentials (str): Path to Google Cloud Storage credentials file.
+    """
     working_dir: Path
     sent_dir: Path
     path_to_gcs_credentials: str
